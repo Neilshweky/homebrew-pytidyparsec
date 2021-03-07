01c4c4cec44462f65770c1342c36b41ddc559043
Neil Shweky <neilshweky@gmail.com>
2021-03-07 02:27:49 -0500

    update for version 0.4.0

--- pytidyparsec.rb
+++ pytidyparsec.rb
@@ -2,7 +2,7 @@ class Pytidyparsec < Formula
   desc "A dead-simple templating utility for simple shell interpolation"
   homepage "https://github.com/Neilshweky/py-parsec"
   url "https://github.com/Neilshweky/homebrew-pytidyparsec/releases/download/0.3.0/pytidyparsec-0.3.0.tar.gz"
-  sha256 "4655fd637150feec97ec2ae19ea06230f9f1aa049237df04b425d6dc39053146"
+  sha256 "8655c14f189a9e7ba1d394faf000127afd226d0fdfc349b2dc560217d32dc12b"
 
   bottle :unneeded
 
